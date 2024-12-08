fe3a84353826ba0a18b6108ddfdbc5f647d225e5
junenghia <nguyenminhtrungnghia@gmail.com>
2024-12-08 18:05:34 +0700

    update code

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse, NextRequest } from "next/server";
 
 const protectedRoute = createRouteMatcher([
   '/',
@@ -8,10 +9,27 @@ const protectedRoute = createRouteMatcher([
   '/personal-room',
 ]);
 
+
+export const actionHeaderCheckOverride = (req: NextRequest) => {
+  console.debug("REQUEST HEADERS:::: ", req.headers);
+
+  const response = NextResponse.next();
+  response.headers.set(
+    "x-forwarded-host",
+    req.headers.get("origin")?.replace(/(http|https):\/\//, "") || "*"
+  );
+  return response;
+};
+
+
 export default clerkMiddleware((auth, req) => {
   if (protectedRoute(req)) auth().protect();
+
+  return actionHeaderCheckOverride(req);
 });
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
