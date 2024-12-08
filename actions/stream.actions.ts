f33234a2595ac774d482b26c238b81e7d0755791
junenghia <nguyenminhtrungnghia@gmail.com>
2024-12-08 17:49:10 +0700

    update code

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -7,28 +7,22 @@ const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const STREAM_API_SECRET = process.env.STREAM_SECRET_KEY;
 
 export const tokenProvider = async () => {
-  const user = await currentUser();
+  try {
+    const user = await currentUser();
 
-  if (!user) {
-    console.error('User is not authenticated');
-    throw new Error('User is not authenticated');
+    if (!user) throw new Error('User is not authenticated');
+    if (!STREAM_API_KEY) throw new Error('Stream API key is missing');
+    if (!STREAM_API_SECRET) throw new Error('Stream API secret is missing');
+
+    const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
+
+    const expirationTime = Math.floor(Date.now() / 1000) + 3600;
+    const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+    const token = streamClient.createToken(user.id, expirationTime, issuedAt);
+    return token;
+  } catch (error) {
+    console.error('Error in tokenProvider:', error);
+    throw new Error('Failed to generate token');
   }
-
-  if (!STREAM_API_KEY) {
-    console.error('Stream API key secret is missing');
-    throw new Error('Stream API key secret is missing');
-  }
-  if (!STREAM_API_SECRET) {
-    console.error('Stream API secret is missing');
-    throw new Error('Stream API secret is missing');
-  }
-
-  const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
-
-  const expirationTime = Math.floor(Date.now() / 1000) + 3600;
-  const issuedAt = Math.floor(Date.now() / 1000) - 60;
-
-  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
-
-  return token;
 };
