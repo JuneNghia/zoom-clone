90a8a3ada165f49b18e3b5e23a60b39b7fbbe4aa
junenghia <nguyenminhtrungnghia@gmail.com>
2024-12-08 18:18:33 +0700

    update code

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,4 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
 const protectedRoute = createRouteMatcher([
   '/',
@@ -9,20 +7,6 @@ const protectedRoute = createRouteMatcher([
   '/previous',
   '/personal-room',
 ]);
-
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-
-  // Sửa `x-forwarded-host` để khớp với `origin`
-  const origin = req.headers.get('origin');
-  const response = NextResponse.next();
-
-  if (origin) {
-    response.headers.set('x-forwarded-host', new URL(origin).host);
-  }
-
-  return response;
-}
 
 export default clerkMiddleware((auth, req) => {
   if (protectedRoute(req)) auth().protect();
