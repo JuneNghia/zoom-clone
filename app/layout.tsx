1a50154661d9885e690f2989d0e08e8d7e78ec79
junenghia <nguyenminhtrungnghia@gmail.com>
2025-03-20 23:10:55 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
   return (
     <ClerkProvider
       localization={viVN}
-      afterSignUpUrl={'/'}
       appearance={{
         layout: {
           socialButtonsVariant: 'blockButton',
