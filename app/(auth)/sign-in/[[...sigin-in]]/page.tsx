391739de052897798d45c7ddede59fbffa81204f
junenghia <nguyenminhtrungnghia@gmail.com>
2025-03-26 11:30:42 +0700

    update

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -14,7 +14,7 @@ export default function SiginInPage() {
 
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <SignIn afterSignInUrl={'/'}/>
+      <SignIn forceRedirectUrl={'/'}/>
     </main>
   );
 }
